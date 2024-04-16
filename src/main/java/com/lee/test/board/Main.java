c39c8c4299751cdae6c74e8fbf16805973e1b5d9
DonghyeokL <taroian17@daum.net>
2024-04-16 19:23:24 +0900

    프로그램 시작 메시지 출력

--- src/main/java/com/lee/test/board/Main.java
+++ src/main/java/com/lee/test/board/Main.java
@@ -2,6 +2,10 @@ package com.lee.test.board;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("== 텍스트 게시판(ver 0.1) ==");
+    System.out.println("프로그램 시작");
+    System.out.println("프로그램 종료");
+    System.out.println("== 게시판을 종료합니다 ==");
   }
 }
+
