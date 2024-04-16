8ede608b33dc9a45a8f4ce5973e776ebe77e97ea
DonghyeokL <taroian17@daum.net>
2024-04-16 19:55:39 +0900

    게시판 기능 추가

--- src/main/java/com/lee/test/board/Main.java
+++ src/main/java/com/lee/test/board/Main.java
@@ -1,11 +1,51 @@
 package com.lee.test.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("== 텍스트 게시판(ver 0.1) ==");
+    Scanner sc = new Scanner(System.in);
+    int articleLastId = 0;
+
+    System.out.println("== 텍스트 게시판 v 0.1 ==");
     System.out.println("프로그램 시작");
-    System.out.println("프로그램 종료");
-    System.out.println("== 게시판을 종료합니다 ==");
+
+    while (true) {
+      System.out.printf("명령) ");
+      String cmd = sc.nextLine();
+
+      if(cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 작성 ==");
+
+        System.out.printf("제목) ");
+        String title = sc.nextLine();
+
+        System.out.printf("내용) ");
+        String body = sc.nextLine();
+
+        int id = ++articleLastId;
+
+        Article article = new Article();
+
+
+        System.out.println("생성된 게시물 객체 : " + article);
+
+        System.out.printf("%d번 게시물이 생성되었습니다.\n", article.id);
+      }
+
+      else if(cmd.equals("exit")) {
+        System.out.println("== 게시판을 종료합니다 ==");
+        break;
+      }
+    }
+
+    sc.close();
   }
 }
 
+class Article {
+  int id;
+  String title;
+  String body;
+
+}
